da952c6d5e25a16f3531b6de89b9a8d9d8e2caea
Cesar Zabala <cesarzabala@gmail.com>
2022-03-24 19:09:49 -0500

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
+	roots: ['<rootDir>/src'],
+	coverageThreshold: {
+		'./src': {
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: 80,
+		},
+	},
 };
